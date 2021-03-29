c8fe9618576952a0aeae7c1ebe17bc25b0bc737f
Adam <adam.nemeth.0315@gmail.com>
2021-03-29 12:25:28 +0200

    alapok kész vannak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { AdminComponent } from './page/admin/admin.component';
+import { QuizeditorComponent } from './page/quizeditor/quizeditor.component';
+import { QuestioneditorComponent } from './page/questioneditor/questioneditor.component';
+import { QuizComponent } from './page/quiz/quiz.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    QuizeditorComponent,
+    QuestioneditorComponent,
+    QuizComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
