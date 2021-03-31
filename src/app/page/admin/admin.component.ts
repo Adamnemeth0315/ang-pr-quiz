ee35f0811014999c4a01214d26b6c9c0a96179ac
Adam <adam.nemeth.0315@gmail.com>
2021-03-31 09:53:28 +0200

    Táblázat kész szűrés keresés megy

--- src/app/page/admin/admin.component.ts
+++ src/app/page/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Quiz } from 'src/app/model/quiz';
+import { QuizService } from 'src/app/service/quiz.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  quizList$: BehaviorSubject<Quiz[]> = this.quizService.list$;
+
+  phrase: string = '';
+  filterKey: string = 'title';
+  sorterKey: string = '';
+  sorterDirection: number = 1;
+
+  constructor(
+    private quizService: QuizService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.quizService.getAll();
+  }
+
+  onRemove(quiz: Quiz): void {
+    this.quizService.remove(quiz).subscribe(
+      () => {
+        this.quizService.getAll();
+        this.router.navigate(['/admin']);
+      }
+    )
+  }
+
+  onChangePhrase(event: Event): void {
+    this.phrase = (event.target as HTMLInputElement).value;
+  }
+
+  onSort(key: string): void {
+    if (key === this.sorterKey) {
+      this.sorterDirection *= -1;
+    } else {
+      this.sorterDirection = 1;
+    }
+
+    this.sorterKey = key;
   }
 
 }
